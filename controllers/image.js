2f3c113fe29b92ff4c36d97824f8b7920badd104
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-07-29 05:04:17 +0530

    SECURITY REVIEW

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,21 @@
 
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'ee2a54a5c60a4d13a669374dcaec43b7'
+   });
+
+
+const handleApiCall = (req,res) =>{
+    app.models
+   .predict( Clarifai.FACE_DETECT_MODEL, req.body.input)
+   .then(data =>{
+       res.json(data);
+   })
+   .catch(err=>res.status(400).json('unable to fetch API'))
+
+}
+
 const handleImage =(req,res,db) =>{
     const {id} = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +27,6 @@ const handleImage =(req,res,db) =>{
     .catch(err => res.status(400).json('unable to get entries'))
 }
 module.exports = {
-    handleImage: handleImage            // can also use es6 syntax
+    handleImage,
+    handleApiCall           // can also use es6 syntax
 };
